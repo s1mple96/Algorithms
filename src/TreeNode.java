4faada0b3ceb65961c430c8837dd425010e8493e
s1mple <903255458@qq.com>
2021-05-15 20:38:16 -0800

    二叉树

---
+++ src/TreeNode.java
@@ -0,0 +1,22 @@
+/**
+ * @author s1mple
+ * @create 2021/5/15-20:36
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode() {
+    }
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+
+    public TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
