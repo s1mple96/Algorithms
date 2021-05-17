12bd674408e74d9fe9bdaaa254cee509ebf2172a
s1mple96 <51851184+s1mple96@users.noreply.github.com>
2021-05-17 13:42:59 +1000

    动态规划求解不同路径II

---
+++ src/dp/GetUniquePathsWithObstacles.java
@@ -0,0 +1,38 @@
+package dp;
+
+/**
+ * @author s1mple
+ * @create 2021/5/17-13:34
+ */
+public class GetUniquePathsWithObstacles {
+
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        int[][] dp = new int[m][n];
+        //第一列初始化
+        for (int i = 0; i < m; i++) {
+            if (obstacleGrid[i][0] == 0) {
+                dp[i][0] = 1;
+            } else {
+                break;
+            }
+        }
+        //第一行初始化
+        for (int i = 0; i < n; i++) {
+            if (obstacleGrid[0][i] == 0) {
+                dp[0][i] = 1;
+            } else {
+                break;
+            }
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (obstacleGrid[i][j] == 0) {
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                }
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+}
