7c25543c61d0ee655438ac0047ddef6e1904f487
s1mple96 <51851184+s1mple96@users.noreply.github.com>
2021-05-17 17:20:39 +1000

    最长公共子串和子序列

---
+++ src/dp/GetMaxLong.java
@@ -0,0 +1,26 @@
+package dp;
+
+/**
+ * @author s1mple
+ * @create 2021/5/17-17:10
+ */
+public class GetMaxLong {
+    public static int maxLong(String str1, String str2) {
+        if (str1 == null || str2 == null || str1.length() == 0 || str2.length() == 0) {
+            return 0;
+        }
+        int[][] dp = new int[str1.length() + 1][str2.length() + 1];
+        int max = 0;
+        for (int i = 1; i < str1.length(); i++) {
+            for (int j = 1; j < str2.length(); j++) {
+                if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = 0;
+                }
+                max = Math.max(max, dp[i][j]);
+            }
+        }
+        return max;
+    }
+}
