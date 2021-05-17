a27dd337d3ed66521e958ef8633da3aedd71ffd0
s1mple96 <51851184+s1mple96@users.noreply.github.com>
2021-05-17 16:15:10 +1000

    动态规划和滑动窗口解决最长重复子数组

---
+++ src/dp/GetFindLength.java
@@ -0,0 +1,22 @@
+package dp;
+
+/**
+ * @author s1mple
+ * @create 2021/5/17-13:52
+ */
+public class GetFindLength {
+
+    public int findLength(int[] A, int[] B) {
+        int max = 0;
+        int[][] dp = new int[A.length + 1][B.length + 1];
+        for (int i = 1; i <= A.length; i++) {
+            for (int j = 1; j <= B.length; j++) {
+                if (A[i - 1] == B[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                    max = Math.max(max, dp[i][j]);
+                }
+            }
+        }
+        return max;
+    }
+}
