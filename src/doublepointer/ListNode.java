242725563e28913ee432af5498838b30ce07a649
s1mple96 <51851184+s1mple96@users.noreply.github.com>
2021-05-18 19:26:11 +1000

    ListNode

---
+++ src/doublepointer/ListNode.java
@@ -0,0 +1,15 @@
+package doublepointer;
+
+/**
+ * @author s1mple
+ * @create 2021/5/18-19:25
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+
+}
