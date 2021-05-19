eda9f056452e5d4128d7f06f08247810cc328114
s1mple96 <51851184+s1mple96@users.noreply.github.com>
2021-05-19 18:59:29 +1000

    反转链表第一种方式(栈) 反转链表第二种方式(双指针)

--- src/linkedlist/ReverseListDemo.java
+++ src/linkedlist/ReverseListDemo.java
@@ -9,6 +9,33 @@ import java.util.Stack;
  * @create 2021/5/19-17:47
  */
 public class ReverseListDemo {
+
+    /**
+     * 双链表方法
+     * @param head
+     * @return
+     */
+    public ListNode reverseList2(ListNode head) {
+        //申请节点，pre和 cur，pre指向null
+        ListNode pre = null;
+        ListNode cur = head;
+        while(cur!=null) {
+            //记录当前节点的下一个节点
+            ListNode tmp = cur.next;
+            //然后将当前节点指向pre
+            cur.next = pre;
+            //pre和cur节点都前进一位
+            pre = cur;
+            cur = tmp;
+        }
+        return pre;
+    }
+
+    /**
+     * 栈方法
+     * @param head
+     * @return
+     */
     public ListNode reverseList(ListNode head) {
         Stack<ListNode> stack = new Stack<>();
         //把链表节点全部摘掉放到栈中
