1e5b1485cfd1711602cfb12e428b9a038e427d01
s1mple96 <51851184+s1mple96@users.noreply.github.com>
2021-05-20 16:25:27 +1000

    找出两个链表的第一个公共节点(三种解法)

--- src/linkedlist/GetIntersectionNodeDemo.java
+++ src/linkedlist/GetIntersectionNodeDemo.java
@@ -18,6 +18,27 @@ public class GetIntersectionNodeDemo {
      * @return
      */
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        //tempA和tempB我们可以认为是A,B两个指针
+        ListNode tempA = headA;
+        ListNode tempB = headB;
+        while (tempA != tempB) {
+            //如果指针tempA不为空,tempA就往前移一步
+            //如果tempA为空,就让指针tempA指向tempB
+            tempA = tempA == null ? headB : tempA.next;
+            //指针tempB同上
+            tempB = tempB == null ? headA : tempB.next;
+        }
+        //tempA要么是空的,要么是两个链表的交点
+        return tempA;
+    }
+
+    /**
+     * 链表的长度
+     * @param headA
+     * @param headB
+     * @return
+     */
+    public ListNode getIntersectionNode3(ListNode headA, ListNode headB) {
         //统计链表的A和链表B的长度
         int lenA = length(headA);
         int lenB = length(headB);
