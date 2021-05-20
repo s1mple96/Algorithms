fedd9593faf57243f304099b202eee95903c4a22
s1mple96 <51851184+s1mple96@users.noreply.github.com>
2021-05-20 13:59:53 +1000

    找出两个链表的第一个公共节点

--- src/linkedlist/GetIntersectionNodeDemo.java
+++ src/linkedlist/GetIntersectionNodeDemo.java
@@ -11,7 +11,57 @@ import java.util.Set;
  */
 public class GetIntersectionNodeDemo {
 
+    /**
+     * 双指针方法
+     * @param headA
+     * @param headB
+     * @return
+     */
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        //统计链表的A和链表B的长度
+        int lenA = length(headA);
+        int lenB = length(headB);
+
+        //如果长度不一样,节点多的先走,直到他们长度相同为止
+        while (lenA != lenB) {
+            if (lenA > lenB) {
+                //如果链表A长,那么链表A先走
+                headA = headA.next;
+                lenA--;
+            } else {
+                headB = headB.next;
+                lenB--;
+            }
+        }
+        //然后开始比较,如果他两不相等一直走下去
+        while (headA.val != headB.val) {
+            headA = headA.next;
+            headB = headB.next;
+        }
+        /**
+         * 走到最后,最终会有两种可能,一直是headA为空,一种就是headA不为空有交点
+         */
+        return headA;
+    }
+
+    //统计链表的长度
+    private int length(ListNode node) {
+        int length = 0;
+        while (node != null) {
+            node = node.next;
+            length++;
+        }
+        return length;
+    }
+
+
+    /**
+     * set方法
+     * @param headA
+     * @param headB
+     * @return
+     */
+    public ListNode getIntersectionNode2(ListNode headA, ListNode headB) {
         //创建集合set
         Set<ListNode> set = new HashSet<>();
         //先把链表A的节点全部存放到集合set中
