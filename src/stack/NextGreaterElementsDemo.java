9558b8bfaec88574105cb162d053a1abdb7ed4e8
s1mple96 <51851184+s1mple96@users.noreply.github.com>
2021-05-22 13:18:10 +1000

    单调栈解下一个更大元素 II

---
+++ src/stack/NextGreaterElementsDemo.java
@@ -0,0 +1,38 @@
+package stack;
+
+import java.util.Arrays;
+import java.util.Stack;
+
+/**
+ * @author s1mple
+ * @create 2021/5/22-12:26
+ */
+public class NextGreaterElementsDemo {
+    public int[] nextGreaterElements(int[] nums) {
+        int length = nums.length;
+        int res[] = new int[length];
+        Arrays.fill(res, -1);//默认为都是-1
+        Stack<Integer> stack = new Stack<>();
+        //相当于把数组循环两遍
+        for (int i = 0; i < length * 2; i++) {
+            /**
+             * 遍历数组的第index(index从0开始)个元素,因为数组会遍历两遍
+             * ,会导致数组越界异常,所以这里要对数组长度进行求余
+             */
+            int index = i % length;
+            /**
+             * 单调栈,他存储的是元素的下标,不是元素具体值,从栈顶到
+             * 栈底所对应的值是递增的(栈顶元素在数组中对应的值最小,
+             * 栈底元素对应的值最大),如果栈顶元素对应的值比nums[index]小
+             * 说明栈顶元素对应的值遇到了右边第一个比他大的值,然后栈顶元素出栈,
+             * 让他对应的位置变为nums[index],也就是他右边第一个比他大的值,然后继续判断.
+             */
+            while (!stack.isEmpty() && nums[stack.peek()] < nums[index]) {
+                res[stack.pop()] = nums[index];
+            }
+            //当前元素的下标入栈
+            stack.push(index);
+        }
+        return res;
+    }
+}
