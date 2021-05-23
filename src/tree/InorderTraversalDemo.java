87964a400dfc3dd984ffd1a44cd8be886b261241
s1mple <903255458@qq.com>
2021-05-22 22:28:42 -0800

    二叉树的Morris中序和前序遍历

--- src/tree/InorderTraversalDemo.java
+++ src/tree/InorderTraversalDemo.java
@@ -52,4 +52,43 @@ public class InorderTraversalDemo {
         return res;
     }
 
+    /**
+     * Morris前序遍历
+     * @param root
+     * @return
+     */
+    public List<Integer> preOrderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        //首先把根节点赋值给cur
+        TreeNode cur = root;
+        //如果cur不为空就继续遍历
+        while (cur != null) {
+            if (cur.left == null) {
+                //如果当前节点cur的左子节点为空,就访问当前节点cur
+                //接着让当前节点cur指向他的右子节点
+                res.add(cur.val);
+                cur = cur.right;
+            } else {
+                TreeNode pre = cur.left;
+                //查找pre节点,注意这里有个判断就是pre的右子节点不能等于cur
+                while (pre.right != null && pre.right != cur) {
+                    pre = pre.right;
+                }
+                //如果pre节点的右指针指向空,我们就让他指针指向当前节点cur
+                //然后打印当前节点cur的值,最后再让当前节点cur指向他的左子节点
+                if (pre.right == null) {
+                    pre.right = cur;
+                    res.add(cur.val);
+                    cur = cur.left;
+                } else {
+                    //如果pre节点的右指针不为空,那么他肯定是指向cur的
+                    //表示当前节点cur和他的左子节点都遍历完了,直接让他指向的右子节点即可
+                    pre.right = null;
+                    cur = cur.right;
+                }
+            }
+        }
+        return res;
+    }
+
 }
