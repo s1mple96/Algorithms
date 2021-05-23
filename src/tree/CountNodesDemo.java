80104b63c1b24fc66efbfd5c37217a60687d695c
s1mple <903255458@qq.com>
2021-05-22 23:01:44 -0800

    完全二叉树的节点个数

---
+++ src/tree/CountNodesDemo.java
@@ -0,0 +1,47 @@
+package tree;
+
+import 数据结构.TreeNode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * @author s1mple
+ * @create 2021/5/22-22:53
+ */
+public class CountNodesDemo {
+    /**
+     * BFS解决
+     * @param root
+     * @return
+     */
+    public int countNodes(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int count = 0;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            //poll方法相当于移除队列头部的元素
+            TreeNode node = queue.poll();
+            count++;//统计节点个数
+            if (node.left != null) {
+                queue.add(node.left);
+            }
+            if (node.right != null) {
+                queue.add(node.right);
+            }
+        }
+        return count;
+    }
+
+    /**
+     * DFS解决:直接返回 左节点个数+右节点个数 + 1
+     * @param root
+     * @return
+     */
+    public int countNodes2(TreeNode root) {
+        return root == null ? 0 : countNodes2(root.left) + countNodes2(root.right) + 1;
+    }
+}
