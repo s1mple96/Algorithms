408c83056a848c02a7b80058500a36c8895829fd
s1mple <903255458@qq.com>
2021-05-24 06:25:27 -0800

    剑指 Offer-树的子结构

---
+++ src/tree/IsSubStructureDemo.java
@@ -0,0 +1,34 @@
+package tree;
+
+import 数据结构.TreeNode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * @author s1mple
+ * @create 2021/5/24-6:09
+ */
+public class IsSubStructureDemo {
+
+    public boolean isSubStructure(TreeNode A, TreeNode B) {
+        //边界条件判断,如果A和B有一个为空,返回false
+        if (A == null || B == null) {
+            return false;
+        }
+        return isSub(A, B) || isSubStructure(A.left, B) || isSubStructure(A.right, B);
+    }
+
+    private boolean isSub(TreeNode A, TreeNode B) {
+        //这里如果B为空,说明B已经访问完了.确定是A的子结构
+        if (B == null) {
+            return true;
+        }
+        //如果B不为空A为空,或者两个节点值不相同,说明B树不是A的子结构,直接返回false
+        if (A == null || A.val != B.val) {
+            return false;
+        }
+        //当前节点比较完之后还需要继续判断左右子节点
+        return isSub(A.left, B.left) && isSub(A.right, B.right);
+    }
+}
