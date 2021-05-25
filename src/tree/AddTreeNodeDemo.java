303ef625c82052cf8922f1a7a52a84cd965f8d73
s1mple96 <51851184+s1mple96@users.noreply.github.com>
2021-05-25 19:33:35 +1000

    二叉搜索树中插入节点

---
+++ src/tree/AddTreeNodeDemo.java
@@ -0,0 +1,36 @@
+package tree;
+
+import 数据结构.TreeNode;
+
+/**
+ * @author s1mple
+ * @create 2021/5/25-19:25
+ */
+public class AddTreeNodeDemo {
+    public static void addTreeNode(TreeNode root, int data) {
+        TreeNode node = new TreeNode(data);
+        TreeNode p = root;
+        while (true) {
+            /**
+             * 如果要插入的节点data比节点p的值小,就往p节点的
+             * 左子节点找,否则往p的右子节点找
+             */
+            if (p.val > data) {
+                //如果p的左子节点为空,直接放进去
+                if (p.left == null) {
+                    p.left = node;
+                    break;
+                } else {
+                    p = p.left;
+                }
+            } else {
+                if (p.right == null) {
+                    p.right = node;
+                    break;
+                } else {
+                    p = p.right;
+                }
+            }
+        }
+    }
+}
