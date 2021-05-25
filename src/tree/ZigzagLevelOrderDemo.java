0d0d3f840d7e850066225748f19e95652aa85eaf
s1mple96 <51851184+s1mple96@users.noreply.github.com>
2021-05-25 18:59:39 +1000

    二叉树的锯齿形层次遍历

--- src/tree/ZigzagLevelOrderDemo.java
+++ src/tree/ZigzagLevelOrderDemo.java
@@ -3,9 +3,7 @@ package tree;
 import 数据结构.TreeNode;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Queue;
 
 /**
  * @author s1mple
@@ -26,11 +24,93 @@ public class ZigzagLevelOrderDemo {
     }
 
     /**
-     * BFS打印
+     * DFS解法
      * @param root
      * @return
      */
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        travel(root, res, 0);
+        return res;
+    }
+
+    private void travel(TreeNode cur, List<List<Integer>> res, int level) {
+        if (cur == null) {
+            return;
+        }
+        //如果res.size()<=level说明下一层的集合还没创建,所以要先创建下一层的集合
+        List<Integer> list = res.get(level);
+        /**
+         * 这里默认根节点是第0层,偶数层相当于从左往右遍历
+         * 所以要添加到集合的末尾,如果是奇数层相当于从右往左遍历
+         * 要把数据添加到集合的开头
+         */
+        if (level % 2 == 0) {
+            list.add(cur.val);
+        } else {
+            list.add(0, cur.val);
+        }
+        //分别遍历左右两个子节点,到下一层了,所以层数要加1
+        travel(cur.left, res, level + 1);
+        travel(cur.right, res, level + 1);
+    }
+
+    /**
+     * BFS双端队列
+     * @param root
+     * @return
+     */
+    /*public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+        //双端队列,两边都可以操作
+        Deque<TreeNode> deque = new LinkedList<>();
+        //添加到队列的头
+        deque.addFirst(root);
+        while (!deque.isEmpty()) {
+            List<Integer> level = new ArrayList<>();
+            //统计这一行有多少个节点
+            int count = deque.size();
+            //遍历这一行的所有节点
+            TreeNode cur;
+            for (int i = 0; i < count; i++) {
+                if (res.size() % 2 == 1) {
+                    //从左往右打印
+                    //移除队列头部的元素,如果子节点不为空加入到队列的尾部
+                    cur = deque.removeFirst();
+                    if (cur.right != null) {
+                        deque.addLast(cur.right);
+                    }
+                    if (cur.left != null) {
+                        deque.addLast(cur.left);
+                    }
+                } else {
+                    //从右往左打印
+                    //移除队列尾部的元素,如果子节点不为空加入到队列的头部
+                    cur = deque.removeLast();
+                    if (cur.left != null) {
+                        deque.addFirst(cur.left);
+                    }
+                    if (cur.right != null) {
+                        deque.addFirst(cur.right);
+                    }
+                }
+                level.add(cur.val);
+            }
+            res.add(level);
+        }
+        return res;
+    }*/
+
+
+    /**
+     * BFS打印
+     * @param root
+     * @return
+     */
+    /*public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
         List<List<Integer>> res = new ArrayList<>();
         if (root == null) {
             return res;
@@ -64,5 +144,5 @@ public class ZigzagLevelOrderDemo {
             left2Right = !left2Right;
         }
         return res;
-    }
+    }*/
 }
