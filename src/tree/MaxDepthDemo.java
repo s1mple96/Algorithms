01ae5d53ff528a656970fb695eae55f8214eb688
s1mple <903255458@qq.com>
2021-05-25 05:34:59 -0800

    二叉树的最大深度

---
+++ src/tree/MaxDepthDemo.java
@@ -0,0 +1,39 @@
+package tree;
+
+import 数据结构.TreeNode;
+
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * @author s1mple
+ * @create 2021/5/25-5:12
+ */
+public class MaxDepthDemo {
+    public int maxDepth(TreeNode root) {
+        if (root == null)
+            return 0;
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+}
+    /*public int maxDepth(TreeNode root) {
+        if (root == null)
+            return 0;
+        Deque<TreeNode> stack = new LinkedList<>();
+        stack.push(root);
+        int count = 0;
+        while (!stack.isEmpty()) {
+            int size = stack.size();
+            while (size-- > 0) {
+                TreeNode node = stack.pop();
+                if (node.left!=null)
+                    stack.addLast(node.left);
+                if (node.right!=null)
+                    stack.addLast(node.right);
+            }
+            count++;
+        }
+        return count;
+    }*/
+
