d8730ffa0493490195e372d208f45e70e66dd338
s1mple96 <51851184+s1mple96@users.noreply.github.com>
2021-05-25 13:03:02 +1000

    剑指 Offer-重建二叉树

---
+++ src/tree/BuildTreeDemo.java
@@ -0,0 +1,32 @@
+package tree;
+
+
+import 数据结构.TreeNode;
+
+/**
+ * @author s1mple
+ * @create 2021/5/25-12:59
+ */
+public class BuildTreeDemo {
+
+    private int in = 0;
+    private int pre = 0;
+
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        return build(preorder, inorder, Integer.MIN_VALUE);
+    }
+
+    private TreeNode build(int[] preorder, int[] inorder, int stop) {
+        if (pre >= preorder.length) {
+            return null;
+        }
+        if (inorder[in] == stop) {
+            in++;
+            return null;
+        }
+        TreeNode node = new TreeNode(preorder[pre++]);
+        node.left = build(preorder, inorder, node.val);
+        node.right = build(preorder, inorder, stop);
+        return node;
+    }
+}
