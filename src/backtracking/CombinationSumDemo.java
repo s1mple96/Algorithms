8659ba11624cb7f3abb105e674c79ca963e8cf16
s1mple96 <51851184+s1mple96@users.noreply.github.com>
2021-05-27 18:09:53 +1000

    回溯算法求组合问题

--- src/backtracking/CombinationSumDemo.java
+++ src/backtracking/CombinationSumDemo.java
@@ -1,6 +1,7 @@
 package backtracking;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -14,6 +15,7 @@ import java.util.List;
 public class CombinationSumDemo {
     public static List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(candidates);//先排序
         backtrack(result, new ArrayList<>(), candidates, target, 0);
         return result;
     }
@@ -29,10 +31,24 @@ public class CombinationSumDemo {
             if (target < candidates[i]) {
                 continue;
             }
+            if (i > start && candidates[i] == candidates[i - 1]) {
+                continue;//去掉重复的
+            }
+            cur.add(candidates[i]);
+            backtrack(result, cur, candidates, target - candidates[i - 1], i + 1);
+            cur.remove(cur.size() - 1);
+
             //由于在java中List是引用传递,所以这里要重新创建一个
-            List<Integer> list = new ArrayList<>(cur);
+            /*List<Integer> list = new ArrayList<>(cur);
             list.add(candidates[i]);
-            backtrack(result, list, candidates, target - candidates[i], i);//遍历3叉树,找到最合适的值
+            backtrack(result, list, candidates, target - candidates[i], i);//遍历3叉树,找到最合适的值*/
         }
     }
+
+    /**
+     * 总结:
+     *  类似这样的题目我们可以把他想象成一颗N叉树,我们先选择一个,然后在递推选择(跟进是否可以选择
+     *  重复的有不同的选择,如果允许有重复的,我们递归的时候还可以再选择当前的,如果不允许有重复的,
+     *  我们递归的时候就从当前的下一个开始选择),沿着这个分支走完之后我们会吧当前节点删除,然后再从下一个分支走下去
+     */
 }
