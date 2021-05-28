ee0ae5b3e5767149d9a4a31d30f387b9ddd6b978
s1mple96 <51851184+s1mple96@users.noreply.github.com>
2021-05-28 17:43:50 +1000

    查找-分块查找

--- src/find/BlockSearchDemo.java
+++ src/find/BlockSearchDemo.java
@@ -6,9 +6,21 @@ package find;
  */
 public class BlockSearchDemo {
     public static void main(String[] args) {
-        int index[] = {22, 48, 86};
+        int[] index = {22, 48, 86};
+        int[] st2 = {22, 12, 13, 8, 9, 20, 33, 42, 38, 24, 48, 60, 58, 74, 49, 86, 53};
+        for (int i = 0; i < 100; i++) {
+            System.out.println(blocksearch(index, st2, i, 6));
+        }
     }
 
+    /**
+     *
+     * @param index 每个元素代表的是每块的最大值
+     * @param arr   代表的是要查找的数组
+     * @param key   代表要查找的元素,m代表每个块大小
+     * @param m
+     * @return
+     */
     public static int blocksearch(int[] index, int[] arr, int key, int m) {
         int i = search(index, key);
         if (i<0)
