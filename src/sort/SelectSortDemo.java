8cc500bdd82da8933049d3d2f88b62d6cf3daed6
s1mple96 <51851184+s1mple96@users.noreply.github.com>
2021-05-29 12:41:24 +1000

    选择排序

--- src/sort/SelectSortDemo.java
+++ src/sort/SelectSortDemo.java
@@ -1,43 +1,33 @@
 package sort;
 
+import java.util.Arrays;
+
 /**
  * @author s1mple
  * @create 2021/5/29-11:26
  */
 public class SelectSortDemo {
     private static void selectSort(int[] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            int index = 1;
-            for (int j = i + 1; j < arr.length; j++) {
-                if (arr[index] > arr[j]) {
-                    index = j;
+        int[] array = Arrays.copyOf(arr, arr.length);
+        //总共要经过N-1轮比较
+        for (int i = 0; i < array.length; i++) {
+            int min = i;
+            //每轮需要比较的次数N-i
+            for (int j = i + 1; j < array.length; j++) {
+                if (arr[j] < arr[min]) {
+                    //记录目前能找到的最小值元素的下标
+                    min = j;
                 }
             }
-            if (i != index) {
-                swap(arr, i, index);
+            //将找到的最小值和i位置所在的值进行交换
+            if (i != min) {
+                int temp = arr[i];
+                arr[i] = arr[min];
+                arr[min] = temp;
             }
         }
     }
 
-    private static void selectSort(int arr[], int index) {
-        if (index == arr.length) {
-            return;
-        }
-        int min = index, i = index + 1;
-        for (; i < arr.length; i++) {
-            if (arr[i] < arr[min]) min = i;
-        }
-        if (min != index) {
-            swap(arr, index, min);
-        }
-        selectSort(arr, ++index);
-    }
-
-    private static void swap(int[] arr, int i, int j) {
-        if (i != j) {
-            int temp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = temp;
-        }
-    }
 }
+
+
