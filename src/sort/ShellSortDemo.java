a597d47ff4be1db1387431e9652f54a83f13c0bd
s1mple96 <51851184+s1mple96@users.noreply.github.com>
2021-05-31 10:53:12 +1000

    希尔排序

---
+++ src/sort/ShellSortDemo.java
@@ -0,0 +1,23 @@
+package sort;
+
+/**
+ * @author s1mple
+ * @create 2021/5/31-10:50
+ */
+public class ShellSortDemo {
+    public static void shellSort(int[] arr) {
+        int length = arr.length;
+        int temp;
+        for (int step = length / 2; step >= 1; step /= 2) {
+            for (int i = step; i < length; i++) {
+                temp = arr[i];
+                int j = i - step;
+                while (j >= 0 && arr[j] > temp) {
+                    arr[j + step] = arr[j];
+                    j -= step;
+                }
+                arr[j + step] = temp;
+            }
+        }
+    }
+}
