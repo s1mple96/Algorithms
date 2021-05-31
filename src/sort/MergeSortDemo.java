2cea9558bd676e7f06e6572e802c3476ac4b1d70
s1mple96 <51851184+s1mple96@users.noreply.github.com>
2021-05-31 11:07:38 +1000

    归并排序

---
+++ src/sort/MergeSortDemo.java
@@ -0,0 +1,43 @@
+package sort;
+
+import java.util.Arrays;
+
+/**
+ * @author s1mple
+ * @create 2021/5/31-11:01
+ */
+public class MergeSortDemo {
+    public int[] sort(int[] sourceArray) {
+        int[] arr = Arrays.copyOf(sourceArray, sourceArray.length);
+        if (arr.length < 2) {
+            return arr;
+        }
+        int middle = (int) Math.floor(arr.length / 2);
+        int[] left = Arrays.copyOfRange(arr, 0, middle);
+        int[] right = Arrays.copyOfRange(arr, middle, arr.length);
+        return merge(sort(left), sort(right));
+    }
+
+    private int[] merge(int[] left, int[] right) {
+        int[] result = new int[left.length + right.length];
+        int i = 0;
+        while (left.length > 0 && right.length > 0) {
+            if (left[0] <= right[0]) {
+                result[i++] = left[0];
+                left = Arrays.copyOfRange(left, 1, left.length);
+            } else {
+                result[i++] = right[0];
+                right = Arrays.copyOfRange(right, 1, right.length);
+            }
+        }
+        while (left.length > 0) {
+            result[i++] = left[0];
+            left = Arrays.copyOfRange(left, 1, left.length);
+        }
+        while (right.length > 0) {
+            result[i++] = right[0];
+            right = Arrays.copyOfRange(right, 1, right.length);
+        }
+        return result;
+    }
+}
