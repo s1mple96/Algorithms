7716647c44e17fe6a12847814e310884c16b4d5e
s1mple96 <51851184+s1mple96@users.noreply.github.com>
2021-06-02 12:38:07 +1000

    两个超级大数相加的问题

--- src/other/AddStringsDemo.java
+++ src/other/AddStringsDemo.java
@@ -1,5 +1,7 @@
 package other;
 
+import java.util.Stack;
+
 /**
  * @author s1mple
  * @create 2021/5/26-13:55
@@ -18,5 +20,29 @@ public class AddStringsDemo {
             carry = sum / 10;
         }
         return sb.reverse().toString();
+    }
+
+    /**
+     *  使用栈方法实现
+     * @param num1
+     * @param num2
+     * @return
+     */
+    public String addStrings2(String num1, String num2) {
+        Stack<Integer> stack = new Stack<>();
+        StringBuilder sb = new StringBuilder();
+        int i = num1.length() - 1;
+        int j = num2.length() - 1;
+        int carry = 0;
+        while (i >= 0 || j >= 0 || carry != 0) {
+            carry += i >= 0 ? num1.charAt(i--) - '0' : 0;
+            carry += j >= 0 ? num2.charAt(j--) - '0' : 0;
+            stack.push(carry % 10);
+            carry = carry / 10;
+        }
+        while (!stack.isEmpty()) {
+            sb.append(stack.pop());
+        }
+        return sb.toString();
     }
 }
