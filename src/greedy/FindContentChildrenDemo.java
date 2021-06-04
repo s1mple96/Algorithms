9dc276dd1e85dff86096c1dbdaa9984d0fb0bfc5
s1mple96 <51851184+s1mple96@users.noreply.github.com>
2021-06-04 16:52:13 +0800

    贪心算法解分发饼干

--- src/greedy/FindContentChildrenDemo.java
+++ src/greedy/FindContentChildrenDemo.java
@@ -5,6 +5,11 @@ import java.util.Arrays;
 /**
  * @author s1mple
  * @create 2021/6/4-16:40
+ * 总结:
+ *  贪心算法只需要满足局部最优解,他只能确定某些问题的可行性范围,
+ *  不能保证解是最佳的.因为贪心算法总是从局部出发,并没有从整体
+ *  考虑,对于有些问题使用贪心算法是可以的,有些是不可以的.这些都要
+ *  根据具体问题具体分析
  */
 public class FindContentChildrenDemo {
     public int findContentChildren(int[] g, int[] s) {
