dddac35df2796f32e2b1dd765aabf150de41cd80
s1mple96 <51851184+s1mple96@users.noreply.github.com>
2021-06-04 15:55:17 +0800

    BFS和动态规划解完全平方数

--- src/DFSandBFS/LevelOrderDemo.java
+++ src/DFSandBFS/LevelOrderDemo.java
@@ -1,9 +1,9 @@
 package DFSandBFS;
 
-import 数据结构.TreeNode;
-
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Set;
 
 /**
  * @author s1mple
@@ -12,29 +12,44 @@ import java.util.Queue;
 public class LevelOrderDemo {
     /**
      * BFS
-     * @param tree
+     * @param n
      */
-    public void levelOrder(TreeNode tree) {
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.add(tree);
-        int level = 0;//统计有多少层
+    public int numSquares(int n) {
+        Queue<Integer> queue = new LinkedList<>();
+        //记录访问过的节点值
+        Set<Integer> visited = new HashSet<>();
+        queue.offer(0);
+        visited.add(0);
+        //树的第几层
+        int level = 0;
         while (!queue.isEmpty()) {
-            //每一层的节点数
+            //每一层的节点数量
             int size = queue.size();
+            level++;
+            //遍历当前层的所有节点
             for (int i = 0; i < size; i++) {
-                TreeNode node = queue.poll();
-                //打印节点
-                System.out.println(node.val);
-                if (node.left != null) {
-                    queue.add(node.left);
-                }
-                if (node.right != null) {
-                    queue.add(node.right);
+                //节点的值
+                int digit = queue.poll();
+                //访问当前节点的子节点,类比于二叉树的左右子节点
+                for (int j = 1; j <= n; j++) {
+                    //子节点的值
+                    int nodeValue = digit + j * j;
+                    //nodeValue始终是完全平方数的和,当他等于n的
+                    //时候,直接返回
+                    if (nodeValue == n) {
+                        return level;
+                    }
+                    //如果大于n.终止内层循环
+                    if (nodeValue > n) {
+                        break;
+                    }
+                    if (!visited.contains(nodeValue)) {
+                        queue.offer(nodeValue);
+                        visited.add(nodeValue);
+                    }
                 }
             }
-            level++;
-            //打印第几层
-            System.out.println(level);
         }
+        return level;
     }
 }
