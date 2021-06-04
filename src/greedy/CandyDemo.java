8db4e9f67cc9b5fdf894fd4de6967fd138cc752b
s1mple96 <51851184+s1mple96@users.noreply.github.com>
2021-06-04 17:02:17 +0800

    分发糖果（贪心算法解决）

---
+++ src/greedy/CandyDemo.java
@@ -0,0 +1,12 @@
+package greedy;
+
+/**
+ * @author s1mple
+ * @create 2021/6/4-17:01
+ */
+public class CandyDemo {
+    public int candy(int[] ratings) {
+        int length = ratings.length;
+        //记录的是从左往右循环的结果
+    }
+}
