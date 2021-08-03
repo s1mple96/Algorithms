1f44cf068403951ee416297e2fdc30db3be63ae4
wangzhiping <243688409@qq.com>
2021-08-03 15:12:16 +0800

    二叉树的中序遍历

---
+++ src/tree/InorderTraversalDemo2.java
@@ -0,0 +1,28 @@
+package tree;
+
+import 数据结构.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 二叉树的中序遍历
+ * @author s1mple
+ * @create 2021/8/3-15:09
+ */
+public class InorderTraversalDemo2 {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        dfs(res, root);
+        return res;
+    }
+
+    private void dfs(List<Integer> res, TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        dfs(res, root.left);
+        res.add(root.val);
+        dfs(res, root.right);
+    }
+}
