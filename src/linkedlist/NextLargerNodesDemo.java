20b652038bfeccc6f57ae0c21327473ddef08058
骆志敏 <243688409@qq.com>
2021-11-02 14:57:39 +0800

    链表中的下一个更大节点

--- src/linkedlist/NextLargerNodesDemo.java
+++ src/linkedlist/NextLargerNodesDemo.java
@@ -84,7 +84,6 @@ public class NextLargerNodesDemo {
         int[] res = new int[list.size()];
         for (int i = 0; i < list.size() - 1; i++) {
             for (int j = i + 1; j < list.size(); j++) {
-
                 if (list.get(j) > list.get(i)) {
                     res[i] = list.get(j);
                     break;
