aece1e06fcbcb249368c2ebd750b174dbc8b04d4
骆志敏 <243688409@qq.com>
2021-11-03 09:48:05 +0800

    删除链表的节点

--- src/linkedlist/DeleteNodeDemo.java
+++ src/linkedlist/DeleteNodeDemo.java
@@ -8,6 +8,26 @@ import doublepointer.ListNode;
  */
 public class DeleteNodeDemo {
 
+    public ListNode deleteNode3(ListNode head, int val) {
+        //初始化一个虚拟节点
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode cur = head;
+        ListNode pre = dummy;
+        while (cur != null) {
+            if (cur.val == val) {
+                //如果找到要删除的节点，直接把他删除
+                pre.next = cur.next;
+                break;
+            }
+            //如果没找到，pre指针和cur指针都同时往后 移动
+            pre = cur;
+            cur = cur.next;
+        }
+        //最后返回虚拟节点的下一个节点即可
+        return dummy.next;
+    }
+
     public ListNode deleteNode(ListNode head, int val) {
         if (head == null) {
             return head;
