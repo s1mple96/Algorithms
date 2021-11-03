161b98367a5fe6bdfb65b7c024c03218912ca35a
骆志敏 <243688409@qq.com>
2021-11-03 15:06:47 +0800

    删除链表的节点

--- src/linkedlist/DeleteNodeDemo.java
+++ src/linkedlist/DeleteNodeDemo.java
@@ -29,12 +29,15 @@ public class DeleteNodeDemo {
     }
 
     public ListNode deleteNode(ListNode head, int val) {
+        //递归的终止条件就是当head等于空的时 候，我们直接返回head，因为一个空的链表我们是没法删除的，也就是下面这样
         if (head == null) {
             return head;
         }
+        //如果head结点不等于空，并且head结点的值等于val，我们直接返回head结点的下一 个结点
         if (head.val == val) {
             return head.next;
         }
+        //否则也就是说头结点是删不掉的，我们就递归调用，从头结点的下一个开始继续上面的 操作，直到删除为止
         head.next = deleteNode(head.next, val);
         return head;
     }
